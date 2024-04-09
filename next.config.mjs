de421247650ea9320f930e8c50937f43eb9e9d66
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2024-04-09 00:59:58 +0000

    Added .nojekyll Built files

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   distDir: "docs",
-  assetPrefix: "/dash-reviews-frontend/",
   basePath: "/dash-reviews-frontend",
   images: { unoptimized: true },
 };
