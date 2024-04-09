83394db886083e70360d567ad1921989754feb21
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2024-04-09 01:08:29 +0000

    Added assetPrefix to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   distDir: "docs",
+  assetPrefix: "/dash-reviews-frontend/",
   basePath: "/dash-reviews-frontend",
   images: { unoptimized: true },
 };
