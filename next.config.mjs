5573c1978f7dae8ef4a52ed93f66c4bc47a8553a
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2024-04-09 00:56:42 +0000

    Built files

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   distDir: "docs",
-  skipTrailingSlashRedirect: true,
   assetPrefix: "/dash-reviews-frontend/",
   basePath: "/dash-reviews-frontend",
   images: { unoptimized: true },
